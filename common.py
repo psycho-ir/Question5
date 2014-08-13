d272def73c2b97d6099890c00726f03622e3b568
soroosh <soroosh.sarabadani@gmail.com>
2014-08-13 12:19:55 +0430

    PostLinkCrawler completed

--- common.py
+++ common.py
@@ -5,9 +5,22 @@ from bs4 import BeautifulSoup
 __author__ = 'soroosh'
 
 
+class Post:
+    def __init__(self, id, name, url, views, category, description, specifications, download_links):
+        self.download_links = download_links
+        self.specifications = specifications
+        self.description = description
+        self.category = category
+        self.views = views
+        self.url = url
+        self.name = name
+        self.id = id
+
+
 class Crawler:
     def _get_soup(self, url):
         response = urllib2.urlopen(urllib.quote(url, safe='/:').encode('utf-8'))
         html = response.read()
         soup = BeautifulSoup(html, from_encoding='utf8')
+        print soup
         return soup
--- post_reader.py
+++ post_reader.py
@@ -1,42 +1,31 @@
+# -*- coding: utf-8 -*-
+from common import Crawler
+
 __author__ = 'soroosh'
 
-import urllib
-import urllib2
-from bs4 import BeautifulSoup
+class PostCrawler(Crawler):
+    def get_post(self, url):
+        post_soup = self._get_soup(url)
+        header = post_soup.header.h1.a.string
+        print header
+        views = post_soup.find('li', {'class': 'hit'})
+        category = reduce(lambda x, y: x + '/' + y, map(lambda a: a.string, post_soup.find('li', {'class': 'cat'}).find_all('a')))
+        description = str(post_soup.find('div', {'id': 'yiv2931868296yui_3_13_0_ym1_1_1389000431840_2408'}))
+        if description == 'None':
+            description = str(post_soup.find('p', {'itemprop': 'text'}))
 
-response = urllib2.urlopen(urllib.quote('http://p30download.com/fa/entry/51105/', safe='/:').encode('utf-8'))
-html = response.read()
-soup = BeautifulSoup(html, from_encoding='utf8')
+        dl_links = post_soup.find('div', {'class': 'download-links'}).find_all('a')
+        titles = post_soup.find('div', {'class': 'extra-info'}).find_all('strong')
+        for t in titles:
+            print {t: t.next_sibling.next_sibling.string}
 
-page_posts = soup.find_all('article', {'class': 'post'})
-first_link = page_posts[0].find('h1').find('a')
-print first_link.get('href')
+        pass
 
-# print(page_posts)
-print 'Number of posts in this page: %s' % str(len(page_posts))
-# first_link.get('href')
-response = urllib2.urlopen(urllib.quote('http://p30download.com/fa/entry/51105/', safe='/:').encode('utf-8'))
-html = response.read()
-soup = BeautifulSoup(html, from_encoding='utf8')
 
-page_post = soup.find('article')
 
-header = page_post.header.h1.a.string
-views = page_post.find('li', {'class': 'hit'})
-url = first_link
-category = reduce(lambda x, y: x + '/' + y, map(lambda a: a.string, page_post.find('li', {'class': 'cat'}).find_all('a')))
-description = str(page_post.find('div', {'id': 'yiv2931868296yui_3_13_0_ym1_1_1389000431840_2408'}))
-if description == 'None':
-    description = str(page_post.find('p', {'itemprop': 'text'}))
-dl_links = page_post.find('div', {'class': 'download-links'}).find_all('a')
-print header
-print views
-print url
-print category
-print description
-print dl_links
-print len(dl_links)
 
+c = PostCrawler()
+c.get_post('http://p30download.com/fa/entry/39455/')
 
 
 
